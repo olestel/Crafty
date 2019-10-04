4dd37422fa3b0f4b6ee6e331730d9c4b78c3fa65
olestel <olestel96@gmail.com>
2019-10-04 12:03:35 +0300

    Fixed buggs in slider and Ñdeveloped Auth block

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let slides = document.getElementsByClassName('authors-slider__item');
 let indicator = document.getElementById('indicator');
 let allIndecators = document.getElementsByClassName('authors-slider__indicator');
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 3000);
+let slideInterval = setInterval(nextSlide, 4000);
 
 for(let i = 0; i < slides.length; i++){
     let indicators = document.createElement('a');
@@ -16,11 +16,13 @@ allIndecators[currentSlide].className = 'authors-slider__indicator selected';
 function nextSlide() {
     for(let j = 0; j < allIndecators.length; j++) {
         allIndecators[j].addEventListener('click', function () {
+            clearInterval(slideInterval);
             slides[currentSlide].className = 'authors-slider__item';
             allIndecators[currentSlide].className = 'authors-slider__indicator';
             slides[j].className = 'authors-slider__item showing';
             allIndecators[j].className = 'authors-slider__indicator selected';
             currentSlide = j;
+            slideInterval = setInterval(nextSlide, 3000);
         })
     }
     console.log(currentSlide);
